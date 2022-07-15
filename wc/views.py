4604a510393301c2a3b9c5b788c4c37e7ccf3776
Glaide <glaide.lara@gmail.com>
2022-07-15 13:11:58 -0300

    improve how to show the message

--- wc/views.py
+++ wc/views.py
@@ -4,15 +4,13 @@ from django.shortcuts import render
 
 
 def text_box(request):
-    message = 0
+    message = ''
     form = FormText()
-
     if request.GET:
-        
-        text = request.GET['words']
-        print(text)
-        countOfWords = len(text.split())
-        if countOfWords != 0:
-            message = countOfWords
+        if form.is_valid:
+            text = request.GET['words']
+            countOfWords = len(text.split())
+            if countOfWords != 0:
+                message = 'The total number of words is: ' + f'{countOfWords}'
        
     return render(request, "init.html",{"message" : message, "form": form})
